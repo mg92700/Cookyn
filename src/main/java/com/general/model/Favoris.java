0c9ed77d20534da90e6a4435556c2c5243142c46
elberkaoui <elberkaoui@DESKTOP-RALG11B.SPIRIT.LOCAL>
2018-10-11 11:07:49 +0200

    Edit Class Favoris

--- src/main/java/com/general/model/Favoris.java
+++ src/main/java/com/general/model/Favoris.java
@@ -24,5 +24,31 @@ public class Favoris implements Serializable {
 	@Id
 	@ManyToOne
 	@JoinColumn(name="idRecette")
-	private Recette recette;	
+	private Recette recette;
+	
+	
+	
+	
+	public Favoris() {
+		super();
+	}
+	public Favoris(User user, Recette recette) {
+		super();
+		this.user = user;
+		this.recette = recette;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Recette getRecette() {
+		return recette;
+	}
+	public void setRecette(Recette recette) {
+		this.recette = recette;
+	}	
+	
+	
 }
