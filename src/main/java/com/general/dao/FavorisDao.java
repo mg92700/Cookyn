2c38f79d3dcf2f0ffc0d4c4460beef09ded30b08
elberkaoui <elberkaoui@DESKTOP-RALG11B.SPIRIT.LOCAL>
2018-10-12 16:06:00 +0200

    Genral fix

--- src/main/java/com/general/dao/FavorisDao.java
+++ src/main/java/com/general/dao/FavorisDao.java
@@ -3,12 +3,8 @@ package com.general.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.general.model.Etape;
 import com.general.model.Favoris;
-import com.general.model.Recette;
 import com.general.model.User;
 
 
