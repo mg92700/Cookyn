f8fe99b2902213933307b93897df529bfdf888fe
elberkaoui <elberkaoui@DESKTOP-RALG11B.SPIRIT.LOCAL>
2018-10-12 15:49:08 +0200

    Planning

--- src/main/java/com/general/model/Favoris.java
+++ src/main/java/com/general/model/Favoris.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
