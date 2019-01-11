107b86da02a1332c77d2dbe244e6e24aa2d8eab7
Polo <paulnaszalyi@yahoo.fr>
2019-01-11 16:26:57 +0100

    Convert unites Paul

--- src/main/java/com/general/service/ConvertUnite.java
+++ src/main/java/com/general/service/ConvertUnite.java
@@ -13,127 +13,39 @@ public class ConvertUnite {
 	UniteDao uniteDao;
 
 	
-	public float Convert(int idUnite,float value)
-	{
-		Unite u= uniteDao.findByidUnite(idUnite);
-		float valueUnite=(float) 0;
-		if(u!=null)
-		{
-			if(u.getLibelleUnite().equals("grammes"))
-			{
-				return value/1000;
-			}
-			else if(u.getLibelleUnite().equals("kilogrammes"))
-			{
-				return value*1000;
-				
-			}
-			else if(u.getLibelleUnite().equals("cuillerée à café"))
-			{
-				return (float) (value*0.5);
-				
-			}
-			else if(u.getLibelleUnite().equals("cuillerée à soupe"))
-			{
-				
-				return (float) (value*1.5);
-				
-			}
-			else if(u.getLibelleUnite().equals("litres"))
-			{
-				return value*1000;
-				
-			}
-			else if(u.getLibelleUnite().equals("centilitre"))
-			{
-				return value/1000;
-				
-			}
-			
+	public float Convert(int idUnite, float value) {
+		Unite u = uniteDao.findByidUnite(idUnite);
+		float valueUnite = (float) 0;
+		if(u != null) {
+			if(u.getLibelleUnite().equals("Grammes")) return value;
+			else if(u.getLibelleUnite().equals("Kilogrammes")) return value * 1000;
+			else if(u.getLibelleUnite().equals("Cuillère à café")) return (float) (value * 0.5);
+			else if(u.getLibelleUnite().equals("Cuillère à soupe")) return (float) (value * 1.5);
+			else if(u.getLibelleUnite().equals("Litres")) return value * 1000;
+			else if(u.getLibelleUnite().equals("Centilitres")) return value * 10;
 		}
 		return valueUnite;
-		
 	}
 	
-	public float ConvertToCentilitre(int idUnite,float value)
-	{
-		Unite u= uniteDao.findByidUnite(idUnite);
-		float valueUnite=(float) 0;
-		if(u!=null)
-		{
-		
-			if(u.getLibelleUnite().equals("cuillerée à café"))
-			{
-				return (float) (value*0.5);
-				
-			}
-			else if(u.getLibelleUnite().equals("cuillerée à soupe"))
-			{
-				
-				return (float) (value*1.5);
-				
-			}
-		/*
-			else if(u.getLibelleUnite().equals("centilitre"))
-			{
-				return value/1000;
-				
-			}
-			*/
+	public float ConvertToCentilitres(int idUnite, float value) {
+		Unite u = uniteDao.findByidUnite(idUnite);
+		float valueUnite = (float) 0;
+		if(u != null) {
+			if(u.getLibelleUnite().equals("Cuillère à café")) return (float) (value * 0.5);
+			else if(u.getLibelleUnite().equals("Cuillère à soupe")) return (float) (value * 1.5);
+			else if(u.getLibelleUnite().equals("Litres")) return (float) (value * 100);
+			else if (u.getLibelleUnite().equals("Centilitres")) return value;
 		}
 		return valueUnite;
-		
 	}
-
-	public float ConvertToLitre(int idUnite,float value)
-	{
-		Unite u= uniteDao.findByidUnite(idUnite);
-		float valueUnite=(float) 0;
-		if(u!=null)
-		{
-			if(u.getLibelleUnite().equals("litres"))
-			{
-				return value*1000;
-				
-			}
-			
-		}
-		return valueUnite;
-		
-	}
-
-	public float ConvertToGramme(int idUnite,float value)
-	{
-		Unite u= uniteDao.findByidUnite(idUnite);
-		float valueUnite=(float) 0;
-		if(u!=null)
-		{
-			if(u.getLibelleUnite().equals("grammes"))
-			{
-				return value/1000;
-			}
-			
-		}
-		return valueUnite;
-		
-	}
-
-	public float ConvertToKilogramme(int idUnite,float value)
-	{
-		Unite u= uniteDao.findByidUnite(idUnite);
-		float valueUnite=(float) 0;
-		if(u!=null)
-		{
-			 if(u.getLibelleUnite().equals("kilogrammes"))
-			{
-				return value*1000;
-				
-			}
 	
+	public float ConvertToGrammes(int idUnite, float value) {
+		Unite u = uniteDao.findByidUnite(idUnite);
+		float valueUnite = (float) 0;
+		if(u != null) {
+			if(u.getLibelleUnite().equals("Grammes")) return value;
+			else if(u.getLibelleUnite().equals("Kilogrammes")) return value * 1000;
 		}
 		return valueUnite;
-		
 	}
-
-
 }
